ee5bc9c45f3a45b97562de26d7aa62dd25aac432
BIT <BIT@DESKTOP-DOGVD1T>
2019-11-21 21:47:13 +0900

    #20191121

---
+++ src/exception/DangerClass.java
@@ -0,0 +1,13 @@
+package exception;
+
+import java.io.IOException;
+
+public class DangerClass {
+
+	public void danger() throws IOException, MyException
+	{
+		System.out.println("Some Code01");
+		System.out.println("Some Code02");
+		
+	}
+}
---
+++ src/exception/MyException.java
@@ -0,0 +1,15 @@
+package exception;
+
+public class MyException extends Exception {
+
+	public MyException()
+	{
+		super("MyException");
+	}
+	
+	public MyException(String message)
+	{
+		super(message);
+	}
+	
+}
